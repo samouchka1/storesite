a73ccdcb8987dc1938b3170a0bb90408f021d193
samouchka1 <mchio421@protonmail.com>
2022-09-24 23:18:14 -0400

    added footer section

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import GridLayout from './GridLayout';
-import Accordion from './Accordion';
+// import Accordion from './Accordion';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <GridLayout />
-      <Accordion />
-      
+      {/* <Accordion /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+    AppBar,
+    Container,
+    Link,
+    List,
+    ListItem,
+    Toolbar,
+    Tooltip,
+    //Typography
+} from '@mui/material';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import RedditIcon from '@mui/icons-material/Reddit';
+
+const socialMedia = [
+    {
+        name: 'Twitter',
+        icon: <TwitterIcon fontSize="large" sx={{color: '#454545'}} />,
+        url: 'https://twitter.com/i/flow/login'
+    },
+    {
+        name: 'Youtube',
+        icon: <YouTubeIcon fontSize="large" sx={{color: '#454545'}} />,
+        url: 'https://www.youtube.com/'
+    },
+    {
+        name: 'Reddit',
+        icon: <RedditIcon fontSize="large" sx={{color: '#454545'}} />,
+        url: 'https://www.reddit.com/'
+    }
+]
+
+//STYLES
+
+const footerStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    width: '100%',
+    padding: '10px',
+    backgroundColor : '#f4f4f4', //background color
+    border: 'solid 1px #b2b2b2',
+    boxShadow: 'none',
+}
+
+const Footer = () => {
+  return (
+    <Container maxWidth={false} sx={{margin: '1rem 0 4rem'}}>
+        <AppBar sx={footerStyles}>
+            <Toolbar>
+                <List sx={{display: 'flex'}}>
+                    {socialMedia.map( link => (
+                        <ListItem key={link} sx={{ margin: '0 1rem'}}>
+                        <Tooltip title={link.name}>
+                            <Link 
+                                alt={link.name}
+                                href={link.url}
+                                target="_blank"
+                            >
+                                {link.icon}
+                            </Link>
+                        </Tooltip>
+                        </ListItem>
+                    ) )}
+                </List>
+            </Toolbar>
+        </AppBar>
+
+    </Container>
+  )
+}
+
+export default Footer;
