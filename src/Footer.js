677083cb931e94ab7c3d6162d05a8c521185fa4b
samouchka1 <mchio421@protonmail.com>
2022-09-24 23:29:46 -0400

    icon color edit

--- src/Footer.js
+++ src/Footer.js
@@ -16,17 +16,17 @@ import RedditIcon from '@mui/icons-material/Reddit';
 const socialMedia = [
     {
         name: 'Twitter',
-        icon: <TwitterIcon fontSize="large" sx={{color: '#454545'}} />,
+        icon: <TwitterIcon fontSize="large" sx={{color: '#535353'}} />,
         url: 'https://twitter.com/i/flow/login'
     },
     {
         name: 'Youtube',
-        icon: <YouTubeIcon fontSize="large" sx={{color: '#454545'}} />,
+        icon: <YouTubeIcon fontSize="large" sx={{color: '#535353'}} />,
         url: 'https://www.youtube.com/'
     },
     {
         name: 'Reddit',
-        icon: <RedditIcon fontSize="large" sx={{color: '#454545'}} />,
+        icon: <RedditIcon fontSize="large" sx={{color: '#535353'}} />,
         url: 'https://www.reddit.com/'
     }
 ]
@@ -39,7 +39,6 @@ const footerStyles = {
     alignItems: 'center',
     position: 'relative',
     width: '100%',
-    padding: '10px',
     backgroundColor : '#f4f4f4', //background color
     border: 'solid 1px #b2b2b2',
     boxShadow: 'none',
